0485e4577c41cca8b5a89bcf31be2fbdc287d4a5
Petr Urbánek <petr.urbanek@solarwinds.com>
2023-08-22 14:19:56 +0200

    create template

---
+++ src/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MathUtilsSimple Math { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Math = new MathUtilsSimple();
+            //Debugger.Launch(); //TODO:: Debugger Launch
+        }
+
+        private void WriteIntoInput(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn is not null)
+            {
+                if (btn.Content.Equals("x"))
+                    this.Input.Text += "*"; 
+                else if (btn.Content.Equals("÷"))
+                    this.Input.Text += "/";
+                else
+                    this.Input.Text += btn.Content;
+            }
+        }
+
+        private void RemoveLastFromInput(object sender, RoutedEventArgs e)
+        {
+            if(this.Input.Text.Length > 0)
+                this.Input.Text = this.Input.Text.Substring(0, this.Input.Text.Length - 1);
+        }
+
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+            this.Input.Text = Math.Evaluate(this.Input.Text);
+        }
+    }
+}
---
+++ src/Mathematics/MathUtilsSimple.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Mathematics
+{
+    public class MathUtilsSimple : MathBase
+    {
+        // WorkShop start Here
+
+        /// <summary>
+        /// Evaluates incoming equation
+        /// Meant to be used with single operation
+        /// </summary>
+        /// <param name="input">incoming equation as string</param>
+        /// <returns>result of evaluation or error message</returns>
+        public string Evaluate(string input)
+        {
+            double a = 0;
+            double b = 0;
+
+            var errorMsg = CheckInputSyntax(input);
+            if (!string.IsNullOrEmpty(errorMsg))
+                return errorMsg;
+
+            var tokens = Tokenize(input);
+            if (tokens.Count > 3)
+            {
+                return "Too many tokens, you may perform only single operation.";
+            }
+
+            double.TryParse(tokens[0], out a);
+            if(tokens.Count == 3)
+                double.TryParse(tokens[2], out b);
+
+            double result = ExecuteSingleOperation(a, b, tokens[1]);
+
+            return result.ToString();
+        }
+
+        //TODO:: (4) implement checkInputSyntax
+        internal string CheckInputSyntax(string input)
+        {
+            return "";
+        }
+
+        //TODO:: (3) use methods from MathBase
+        //TODO:: (5) Add operation for factorial
+        //TODO:: (Optional) Refactor 
+        private double ExecuteSingleOperation(double a, double b, string op)
+        {
+            double result = 0;
+
+            if (op == "+")
+                result = Add(b, a); 
+            if (op == "-")
+                result = a - b;
+            if (op == "/")
+                result = a / b;
+            if (op == "*")
+                result = a * b;
+
+            return result;
+        }
+
+
+    }
+}
